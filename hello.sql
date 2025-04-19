23e5c458581a91f0ff14e094e14e3a5543df6303
raghunadh <44360600+raghunadh10@users.noreply.github.com>
2025-04-19 18:15:25 +0530

    Update hello.sql

--- hello.sql
+++ hello.sql
@@ -2,6 +2,6 @@
 -- It contains characters not supported in Windows-1252
 
 SELECT 'Hello✅ ✅ '; -- Chinese characters
-SELECT 'Rocket'; -- Emoji character
+SELECT 'RocketRocket;🚀 ❌ ✅'; -- Emoji character
 
 

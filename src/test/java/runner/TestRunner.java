7103fdfda9a3fce5498b758781da6b7881c0d7f0
Venkata Raghunadh Reddy <venkataraghunadhreddy@Venkatas-MacBook-Air.local>
2025-04-23 18:04:20 +0530

    allure reporting

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         features = "src/test/java/features",  // Path to feature files
         glue = {"stepdefinitions"},  // Path to step definition files
         plugin = {
-                "pretty",  // Prints readable console output
+                "pretty",// Prints readable console output
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "html:target/cucumber-reports/cucumber.html",  // Generates HTML reports
                 "json:target/cucumber-reports/cucumber.json",  // Generates JSON reports
                 "junit:target/cucumber-reports/cucumber.xml"  // Generates JUnit XML reports
